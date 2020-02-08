0bb77abb1606ac3c43aceed8a1fb79092d2a14c4
Alfira Jessica <alfirajessica@gmail.com>
2020-02-08 12:30:16 +0700

    update login, add register

--- view/home.php
+++ view/home.php
@@ -36,7 +36,7 @@ require_once("head.php");
     <!-- nav -->
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
-            <a class="navbar-brand" href="index.html">Vegefoods</a>
+            <a class="navbar-brand" href="index.html">EMOS</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="oi oi-menu"></span> Menu
             </button>
--- view/register.php
+++ view/register.php
@@ -1,7 +1,7 @@
 <?php
 require_once("head.php");
 ?>
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     
@@ -11,7 +11,7 @@ require_once("head.php");
     <!-- nav -->
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
-            <a class="navbar-brand" href="index.html">Vegefoods</a>
+            <a class="navbar-brand" href="index.html">EMOS</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="oi oi-menu"></span> Menu
             </button>
@@ -26,42 +26,93 @@ require_once("head.php");
     <!-- END nav -->
 
 
+
     <section class="ftco-section contact-section bg-light ">
         <div class="container">
-        <div class="row block-9 justify-content-center">
+            
         
-        <div class="col-md-6 order-md-last d-flex">    
-            <form action="#" class="bg-white p-5 contact-form">
-                <div class="col-md-12 heading-section text-center ftco-animate">
-                    <h3 class="mb-3">Login</h3>
-          	        <span class="subheading">Hello customer, Please fill your account below here</span>            
+        <div class="row block-9 justify-content-center">
+
+        <!-- Profile Usaha beserta pemiliknya -->
+        <div class="col-md-6 d-flex">
+            <form action="#" class="bg-white p-4 contact-form">
+                <div class="form-group">
+                  <h5 for="">Company Profile</h5>
+                  <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="Company Name">
+                  <small id="helpId" class="form-text text-muted">Please fill your company name</small>
                 </div>
 
-                <!-- Login -->
+                <hr>
+
+                <div class="form-group">
+                  <h5 for="">Owner Profile</h5>
+                  <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="Name">
+                  <small id="helpId" class="form-text text-muted">Please fill your name</small>
+                </div>
+
+                <div class="form-group">
+                  <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="example@gmail.com">
+                  <small id="helpId" class="form-text text-muted">Please fill your email</small>
+                </div>
+
+                <div class="form-group">
+                    <input type="number" class="form-control" name="" id="" placeholder="Phone Number">
+                    <small id="helpId" class="form-text text-muted">Please fill your phone number</small>              
+                </div>
+
+                <div class="form-group">
+                    <input type="date" class="form-control" id="" name="bdaytime">
+                    <small id="helpId" class="form-text text-muted">Please fill your birthdate</small>
+                </div>
+
+                <div class="form-group">
+                  <select class="form-control" name="" id="">
+                    <option>Female</option>
+                    <option>Male</option>                   
+                  </select>
+                  <small id="helpId" class="form-text text-muted">Select gender</small>
+                </div>
+
+                <!-- Go to fill username and pass -->
+                <a name="" id="" class="btn btn-primary justify-content-end" href="#" role="button"> <span class="icon-arrow-right"></span> </a>
+                <!-- End Go to fill username and pass -->
+
+            </form>
+        </div>
+        <!-- End Profile Usaha beserta pemiliknya -->
+        
+        <!-- Account -->
+        <div class="col-md-6 order-md-last d-flex">    
+            <form action="#" class="bg-white p-4 contact-form">
+                
+                <div class="form-group">
+                    <h5 for="">Account</h5>          
+                </div>
                 <div class="form-group">
                     <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="Username">
+                    <small id="helpId" class="form-text text-muted">Please fill your Username</small>
                 </div>
                 <div class="form-group">                   
                     <input type="password" class="form-control" name="" id="" placeholder="Password">                 
                 </div>
-                <div class="form-group">            
-                    <input type="number" class="form-control" name="" id="" placeholder="Customer Number">
+                <div class="form-group">                   
+                    <input type="password" class="form-control" name="" id="" placeholder="Confirm Password">                 
                 </div>
                 <div class="form-group justify-content-center">
-                   <button type="button" name="" id="" class="btn btn-primary py-2 px-5">Login</button>
+                   <button type="button" name="" id="" class="btn btn-primary py-2 px-5">Register</button>
                 </div>
-                <!--End Login -->
+
 
                 <hr>
 
-                <!-- Go to Register -->
-                <a href="" class="text-center">Belum punya akun? Register disini! </a>
-                <!-- End Go to Register -->
+                <!-- Go to Register and verification -->
+                <a href="" class="text-center">Dont forget to verify your account. Customer Number will sent to your email </a>
+                <!-- End Go to Register and verification -->
             </form>
 
         </div>
+        <!--End Account -->
 
-          
         </div>
       </div>
         </div>
