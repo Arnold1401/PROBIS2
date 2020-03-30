33be23a685987f945aa5592204339841b8fe4c72
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2020-03-30 19:27:58 +0700

    fix nama perusahaan

--- user-page/ulasan.php
+++ user-page/ulasan.php
@@ -74,7 +74,7 @@ require_once("head.php");
                 <li class="nav-item"><a href="produk.php" class="nav-link">Produk</a></li>
                 <li class="nav-item cta cta-colored"><a href="cart.php" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle active" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">[nama Toko/Perusahaan]</a>
+                <a class="nav-link dropdown-toggle active" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php if(isset($_SESSION["nama_perusahaan"])){ echo $_SESSION["nama_perusahaan"];}?></a>
                 <div class="dropdown-menu" aria-labelledby="dropdown04">
                     <a class="dropdown-item" href="wishlist.php">Daftar Keinginan</a>
                     <a class="dropdown-item" href="status-order.php">Status Order</a>
